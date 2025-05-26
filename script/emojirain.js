7db63daef7202ebcc88aea5bb85cec614d118e15
Caio Carvalho <k4io12345@gmail.com>
2025-05-26 10:53:41 -0300

    change

--- script/emojirain.js
+++ script/emojirain.js
@@ -1,7 +1,7 @@
 const emojiContainer = document.getElementById('emoji-container'); // Cache do container
 let lastScrollY = 0; // Variável para armazenar a última posição de scroll
 
-const emojiCreationInterval = 20; // Crie um emoji a cada 30 frames (ajuste conforme desejado)
+const emojiCreationInterval = 40; // Crie um emoji a cada 30 frames (ajuste conforme desejado)
 
 function createEmoji() {
     const emoji = document.createElement('div');
@@ -14,6 +14,10 @@ function createEmoji() {
     emoji.style.left = Math.random() * window.innerWidth + 'px';
     emoji.style.fontSize = Math.random() * 40 + 20 + 'px';
     emoji.style.animationDuration = Math.random() * 5 + 2 + 's';
+
+    // Adiciona uma inclinação aleatória
+    const randomRotation = Math.random() * 30 - 15; // Gera um ângulo entre -15 e +15 graus
+    emoji.style.transform = `rotate(${randomRotation}deg)`;
 
     // Remover emoji após a animação (ajuste conforme a duração da animação em CSS)
     setTimeout(() => emoji.remove(), 7000);
